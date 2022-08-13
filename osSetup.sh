68faf4d8f1f1b0510375396b827c265000c780c2
jesse <77821870+sunnytower@users.noreply.github.com>
2022-08-13 22:36:42 +0800

    Update osSetup.sh

--- osSetup.sh
+++ osSetup.sh
@@ -12,8 +12,26 @@ case "$(uname -s)" in
     defaults write -g ApplePressAndHoldEnabled -bool false
     #允许任意来源
     sudo spctl --master-disable
+    
+    # take screenshots as jpg (usually smaller size) and not png
+    defaults write com.apple.screencapture type jpg
 
+    # do not open previous previewed files (e.g. PDFs) when opening a new one
+    defaults write com.apple.Preview ApplePersistenceIgnoreState YES
 
+    # show Library folder
+    chflags nohidden ~/Library
+
+    # show hidden files
+    defaults write com.apple.finder AppleShowAllFiles YES
+
+    # show path bar
+    defaults write com.apple.finder ShowPathbar -bool true
+
+    # show status bar
+    defaults write com.apple.finder ShowStatusBar -bool true
+
+    killall Finder;
     ;;
     
     Linux)
