632177661c5c5f505f3be3d5998ffa850c94b03f
jesse <77821870+sunnytower@users.noreply.github.com>
2022-08-13 23:18:02 +0800

    update

--- osSetup.sh
+++ osSetup.sh
@@ -10,21 +10,20 @@ case "$(uname -s)" in
 
     #按住重复按键
     defaults write -g ApplePressAndHoldEnabled -bool false
+    # Set a blazingly fast keyboard repeat rate
+    defaults write NSGlobalDomain KeyRepeat -int 1
+    defaults write NSGlobalDomain InitialKeyRepeat -int 10 
     #允许任意来源
     sudo spctl --master-disable
-    
-    # take screenshots as jpg (usually smaller size) and not png
-    defaults write com.apple.screencapture type jpg
+    # Show the ~/Library folder
+    chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library 
 
     # do not open previous previewed files (e.g. PDFs) when opening a new one
     defaults write com.apple.Preview ApplePersistenceIgnoreState YES
-
+    # Finder: show all filename extensions
+    defaults write NSGlobalDomain AppleShowAllExtensions -bool true
     # show Library folder
     chflags nohidden ~/Library
-
-    # show hidden files
-    defaults write com.apple.finder AppleShowAllFiles YES
-
     # show path bar
     defaults write com.apple.finder ShowPathbar -bool true
 
@@ -32,6 +31,14 @@ case "$(uname -s)" in
     defaults write com.apple.finder ShowStatusBar -bool true
 
     killall Finder;
+    #Disable the sound effects on boot
+    sudo nvram SystemAudioVolume=" " 
+    # Disable the “Are you sure you want to open this application?” dialog
+    defaults write com.apple.LaunchServices LSQuarantine -bool false
+    # Avoid creating .DS_Store files on network or USB volumes
+    defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+    defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+    
     ;;
     
     Linux)
